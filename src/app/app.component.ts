4b2cfb1cbad32e590c759808e1a356fb4e286153
Valdir Ferreira <valdirferreira@Valdir-MacBook-Pro.local>
2021-04-19 00:40:49 -0300

    teste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ColunaModel } from "../models/ColunaModel"
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'MeuProjeto';
+  public colunas: Array<ColunaModel> = [];
+  constructor() {
+
+  }
+
+
+  carregaColunas() {
+    var lista = [];
+    var valores = [60, 9, 100, 20, 75, 33, 39, 3, 18, 59, 33, 44, 12, 10]
+    for (let index = 0; index < 8; index++) {
+      var coluna = new ColunaModel();
+      coluna.porcentagem = valores[index];
+      coluna.descricao = "Descricao - " + index;
+      lista.push(coluna);
+    }
+
+    this.colunas = lista;
+
+  }
+  montaPorcentagemColuna(valor: any) {
+    return valor + "%";
+  }
+
+
+  ngOnInit() {
+    this.carregaColunas();
+  }
+
+
 }
